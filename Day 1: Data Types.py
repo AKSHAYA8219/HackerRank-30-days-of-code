11179226862099a73170e8075a28e31a18721ea0
Akshaya Rajesh <akshayatr1997@gmail.com>
2019-03-22 11:22:31 +0530

    Create Day 1: Data Types.py

---
+++ Day 1: Data Types.py
@@ -0,0 +1,12 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Read and save an integer, double, and String to your variables.
+a = int(input())
+b = float(input())
+c = str(input()) 
+
+print(i+a)
+print(d+b)
+print(s+c)
